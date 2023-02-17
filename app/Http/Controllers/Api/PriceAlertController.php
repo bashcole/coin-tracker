a6dea9d8ab4af4cb4bb489134d5c2d396df62336
Marian <contact@bashcole.com>
2023-02-17 06:27:47 +0200

    Fix dependency

--- app/Http/Controllers/Api/PriceAlertController.php
+++ app/Http/Controllers/Api/PriceAlertController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\CreatePriceAlertRequest;
 use App\Models\PriceAlert;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response;
 
 class PriceAlertController extends Controller
 {
